020a2ad650aa7086f974b2594425b99abab4feff
Rubén <ruben@madteam.cat>
2020-06-10 07:42:15 +0200

    Fase1 ejercicio variables

---
+++ src/Fase1.java
@@ -0,0 +1,26 @@
+/**
+ * @author Rubén Rodríguez
+ *
+ */
+public class Fase1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		String nom = "Rubén", cognom1 = "Rodríguez", cognom2 = "Fernández" ;
+		
+		int dia = 25, mes = 1, any = 1977 ;
+		
+		/*
+		 * Imprimimos por pantalla en dos lineas diferentes las variables
+		 * concatenadas.
+		 */
+		
+		System.out.println(cognom1 + " " + cognom2 + ", " + nom);
+		System.out.println(dia + "/" + mes + "/" + any);
+		
+	}
+
+}
